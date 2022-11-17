cfac942ae857ff14c27d9217536aeef3faa20d51
Thomas Dokas <dokastho@umich.edu>
2022-11-17 22:21:03 +0000

    setup database info

--- homepage/api/pages.py
+++ homepage/api/pages.py
@@ -1,9 +1,9 @@
 import homepage
 
 @homepage.app.route("/api/v1/page/create/", methods=["POST"])
-def create_field():
+def create_page():
     """Create a page."""
 
 @homepage.app.route("/api/v1/page/delete/", methods=["DELETE"])
-def delete_field():
+def delete_page():
     """Delete a page."""
