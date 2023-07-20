4bf61134468b726fec7211d445b5bbf9966df8a0
Thomas Dokas <tjdokas@gmail.com>
2023-07-20 19:44:29 +0000

    Fix DB filename config

--- homepage/config.py
+++ homepage/config.py
@@ -9,7 +9,6 @@ SESSION_COOKIE_NAME = 'login'
 # File Upload to var/uploads/
 SITE_ROOT = pathlib.Path(__file__).resolve().parent.parent
 # Database file is var/resume.sqlite3
-DATABASE_FILENAME = SITE_ROOT/'var'/'homepage.sqlite3'
+DATABASE_FILENAME = 'homepage'
 MY_LOGNAME = "dokastho"
-UPLOAD_FOLDER = SITE_ROOT/'homepage'/'static'/'img'
 NDBS = 3
